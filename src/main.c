1b5cd7cc6f8563d7550d0adb981ce4cac3bd16c2
virgile@rpidemerde <virgile@rpidemerde>
2021-06-06 21:38:27 +0200

    Arduino

--- src/main.c
+++ src/main.c
@@ -5,6 +5,170 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+// Serial Connectivity 
+// 2006-2013, Tod E. Kurt, http://todbot.com/blog/
+
+// Libs Serial 
+#include <stdio.h>    // Standard input/output definitions 
+#include <unistd.h>   // UNIX standard function definitions 
+#include <fcntl.h>    // File control definitions 
+#include <errno.h>    // Error number definitions 
+#include <termios.h>  // POSIX terminal control definitions 
+#include <string.h>   // String function definitions 
+#include <sys/ioctl.h>
+#include <stdint.h>
+int main_fd;
+//  Methods for serial comm up to line 149
+int serialport_init(const char* serialport, int baud)
+{
+    struct termios toptions;
+    int fd;
+    
+    //fd = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
+    fd = open(serialport, O_RDWR | O_NONBLOCK );
+    
+    if (fd == -1)  {
+        perror("serialport_init: Unable to open port ");
+        return -1;
+    }
+    
+    //int iflags = TIOCM_DTR;
+    //ioctl(fd, TIOCMBIS, &iflags);     // turn on DTR
+    //ioctl(fd, TIOCMBIC, &iflags);    // turn off DTR
+
+    if (tcgetattr(fd, &toptions) < 0) {
+        perror("serialport_init: Couldn't get term attributes");
+        return -1;
+    }
+    speed_t brate = baud; // let you override switch below if needed
+    switch(baud) {
+    case 4800:   brate=B4800;   break;
+    case 9600:   brate=B9600;   break;
+#ifdef B14400
+    case 14400:  brate=B14400;  break;
+#endif
+    case 19200:  brate=B19200;  break;
+#ifdef B28800
+    case 28800:  brate=B28800;  break;
+#endif
+    case 38400:  brate=B38400;  break;
+    case 57600:  brate=B57600;  break;
+    case 115200: brate=B115200; break;
+    }
+    cfsetispeed(&toptions, brate);
+    cfsetospeed(&toptions, brate);
+
+    // 8N1
+    toptions.c_cflag &= ~PARENB;
+    toptions.c_cflag &= ~CSTOPB;
+    toptions.c_cflag &= ~CSIZE;
+    toptions.c_cflag |= CS8;
+    // no flow control
+    toptions.c_cflag &= ~CRTSCTS;
+
+    //toptions.c_cflag &= ~HUPCL; // disable hang-up-on-close to avoid reset
+
+    toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+
+    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+    toptions.c_oflag &= ~OPOST; // make raw
+
+    // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
+    toptions.c_cc[VMIN]  = 0;
+    toptions.c_cc[VTIME] = 0;
+    //toptions.c_cc[VTIME] = 20;
+    
+    tcsetattr(fd, TCSANOW, &toptions);
+    if( tcsetattr(fd, TCSAFLUSH, &toptions) < 0) {
+        perror("init_serialport: Couldn't set term attributes");
+        return -1;
+    }
+
+    return fd;
+}
+
+//______________________________________________________________________
+int serialport_close( int fd )
+{
+    return close( fd );
+}
+
+//______________________________________________________________________
+int serialport_writebyte( int fd, uint8_t b)
+{
+    int n = write(fd,&b,1);
+    if( n!=1)
+        return -1;
+    return 0;
+}
+
+//______________________________________________________________________
+int serialport_write(int fd, const char* str)
+{
+    int len = strlen(str);
+    int n = write(fd, str, len);
+    if( n!=len ) {
+        perror("serialport_write: couldn't write whole string\n");
+        return -1;
+    }
+    return 0;
+}
+
+//________________________________________________________________________________
+int serialport_read_until(int fd, char* buf, char until, int buf_max, int timeout)
+{
+    char b[1];  // read expects an array, so we give it a 1-byte array
+    int i=0;
+    do { 
+        int n = read(fd, b, 1);  // read a char at a time
+        if( n==-1) return -1;    // couldn't read
+        if( n==0 ) {
+            usleep( 1 * 1000 );  // wait 1 msec try again
+            timeout--;
+            if( timeout==0 ) return -2;
+            continue;
+        }
+#ifdef SERIALPORTDEBUG  
+        printf("serialport_read_until: i=%d, n=%d b='%c'\n",i,n,b[0]); // debug
+#endif
+        buf[i] = b[0]; 
+        i++;
+    } while( b[0] != until && i < buf_max && timeout>0 );
+
+    buf[i] = 0;  // null terminate the string
+    return 0;
+}
+
+//___________________________________________________________________________
+int serialport_flush(int fd)
+{
+    sleep(2); //required to make flush work, for some reason
+    return tcflush(fd, TCIOFLUSH);
+}
+
+
+char * getResponseCode() {
+	int i;
+    char buffer[100];	
+    //	lecture d'une ligne
+	serialport_read_until(main_fd, buffer, '\r', 99, 10000);
+    // suppression de la fin de ligne
+    for (i=0 ; buffer[i]!='\r' && i<100 ; i++);
+    buffer[i] = 0;
+    //printf("%s", buffer);
+	printf("%s", buffer);
+	char * answer = malloc(sizeof(buffer)*100);
+	strncpy(buffer,answer,100);
+	
+	return answer;
+}
+
+
+
+
+
+
 GObject *p_builder = NULL;
 
 static GtkWidget *createButton(char *label, GCallback callback, gpointer data)
@@ -78,8 +242,11 @@ void set_navigation()
 
 void *handle_fingerprint_detection()
 {
-	g_print("{'type':'fingerprint', 'action':'scan','value':' '}");
-	char *statusmsg = "not ok";
+	//g_print("{'type':'fingerprint', 'action':'scan','value':' '}");
+	serialport_write(main_fd, "{\"type\":\"fingerprint\",\"action\":\"scan\",\"value\":\"\"");
+	sleep(1);
+	getResponseCode();
+	char *statusmsg = "ok";
 	while (strcmp(statusmsg, "ok") != 0)
 	{
 		// retry to scan
@@ -105,8 +272,10 @@ static void *handle_registering()
 		GtkLabel *p_registerLabel = gtk_builder_get_object(p_builder, "registerLabel");
 		gtk_label_set_text(p_registerLabel, "Posez votre doigt puis relacher.");
 		g_print("posez votre doigt\n");
+		getResponseCode();
 		if (i < 3)
 		{
+			
 			i++;
 		}
 		else
@@ -120,7 +289,7 @@ static void *handle_registering()
 	GtkLabel *p_registerLabel = gtk_builder_get_object(p_builder, "registerLabel");
 	gtk_label_set_text(p_registerLabel, "Empreinte enregistré");
 
-	lock();
+	//lock();
 	return NULL;
 }
 
@@ -140,7 +309,8 @@ void lock()
 		// display register page
 		change_page(p_notebook, gtk_builder_get_object(p_builder, "registerPage"));
 
-		g_print("{'type':'fingerprint', 'action':'register','value':' '}");
+		//g_print("{'type':'fingerprint', 'action':'register','value':' '}");
+		serialport_write(main_fd, "{\"type\":\"fingerprint\",\"action\":\"register\",\"value\":\"\"");
 
 		// wait for response in another thread to avoid blocking display
 		pthread_t register_th;
@@ -162,6 +332,10 @@ void lock()
 int main(int argc, char **argv)
 {
 	GError *p_err = NULL;
+
+	// Init Serial
+	main_fd = serialport_init("/dev/ttyACM0", 9600);
+	if (main_fd == -1) {printf("Unable to start serial !\n"); return -1;}
 
 	/* Initialisation de GTK+ */
 	gdk_threads_init();
